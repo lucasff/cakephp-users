609495fe9e7ad5619cedb10aca0b5eaf23c517c4
Lucas Freitas <lucas@lucasfreitas.com.br>
2014-03-07 16:13:02 -0300

    Handling the password hashing and the full name virtual field

--- Model/User.php
+++ Model/User.php
@@ -1,34 +1,30 @@
 <?php
+
 App::uses('UsersAppModel', 'Users.Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
 /**
  * User Model
  *
  * @property Subscription $Subscription
  */
-class User extends UsersAppModel {
+class User extends UsersAppModel
+{
 
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    public $virtualFields = array(
+        'full_name' => 'CONCAT(User.first_name, " ", User.last_name)'
+    );
 
-/**
- * hasMany associations
- *
- * @var array
- */
-	public $hasMany = array(
-		'Subscription' => array(
-			'className' => 'Subscription',
-			'foreignKey' => 'user_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+
+    public function beforeSave($options = array())
+    {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new SimplePasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
+        }
+        return true;
+    }
 
 }
