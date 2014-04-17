d4f60e8b8b31e0b6396a866cf1ca1018ad0061ec
Lucas Freitas <lucas@lucasfreitas.com.br>
2014-04-17 14:22:41 -0300

    Some default

--- Model/UsersAppModel.php
+++ Model/UsersAppModel.php
@@ -4,4 +4,27 @@ App::uses('AppModel', 'Model');
 
 class UsersAppModel extends AppModel {
 
+	/**
+	 * Plugin name
+	 *
+	 * @var string $plugin
+	 */
+	public $plugin = 'Users';
+
+	/**
+	 * Recursive level for finds
+	 *
+	 * @var integer
+	 */
+	public $recursive = -1;
+
+	/**
+	 * Behaviors
+	 *
+	 * @var array
+	 */
+	public $actsAs = array(
+		'Containable'
+	);
+
 }
